1a13e76ae7e64610413dd5198c3d55f5cc9256f2
Frank Davies <fdavies93@gmail.com>
2023-06-09 21:30:32 +0800

    Wrote simple lexer for infix expressions based on formal grammar

--- infix.py
+++ infix.py
@@ -9,63 +9,106 @@ adm = ['+', '/', '*']
 minus = '-'
 dot = '.'
 
-operators = [open_bracket, close_bracket, minus].extend(adm)
-
-class LEX_TYPES(IntEnum):
-    NONE = 0
-    OPERATOR = 1
-    MINUS = 2
-    NUMBER = 3
-    OPEN_BRACKET = 4
-    CLOSE_BRACKET = 5
-
-class LEX_STATE(IntEnum):
-    NEUTRAL = 0
-    PARSING_NUMBER = 1
-
-@dataclass
-class Token():
-    token_type : LEX_TYPES
-    content : str
+operators = [open_bracket, close_bracket, minus]
+operators.extend(adm)
 
 class InfixLexer:
     def __init__(self):
         self.reset()
 
     def reset(self):
-        self.input = input
+        self.tape = ""
         self.tokens = []
-        self.state = LEX_STATE.NEUTRAL
         self.cur_token = ""
+        self.has_dot = False
+        self.step_fn = self.step_neutral
+        self.brackets_open = 0
 
-    def step(self):
-        next_char = self.input[0]
-        self.input = self.input[1:]
-        if self.state == LEX_STATE.NEUTRAL:
-            if next_char in digits or next_char == dot:
+    def step_neutral(self):
+        next_char = self.tape[0]
+        self.tape = self.tape[1:]
+
+        if next_char in digits or next_char == dot:
                 # it's a number
-                self.cur_token += next_char
-                self.state = LEX_STATE.PARSING_NUMBER
-            elif next_char in operators:
-                # decide which class of operator it falls under and reset to neutral
-                pass
-            elif next_char in whitespace:
-                # it's whitespace, do nothing
-                return
-            else:
-                # it's invalid, throw an error
-                raise ValueError
+                self.cur_token = next_char
+                self.step_fn = self.step_lexing_number
+        elif next_char in {minus, open_bracket}:
+            # the only valid operators to start an expression are unary - or (
+            self.tokens.append(next_char)
+            if next_char == open_bracket:
+                self.brackets_open += 1
 
-        elif self.state == LEX_STATE.PARSING_NUMBER:
+        elif next_char in whitespace:
+            # it's whitespace, do nothing
             pass
+        else:
+            # it's invalid, throw an error
+            raise ValueError
+
+    def step_lexing_number(self):
+        next_char = self.tape[0]
+        self.tape = self.tape[1:]
+
+        if next_char in digits:
+            self.cur_token += next_char
+
+        elif next_char == dot and not self.has_dot:
+            self.has_dot = True
+            self.cur_token += next_char
+
+        elif next_char in operators:
+            self.has_dot = False
+            self.tokens.append(self.cur_token)
+            self.tokens.append(next_char)
+            self.cur_token = ""
+            self.step_fn = self.step_neutral
+            if next_char == close_bracket:
+                self.brackets_open -= 1
+                self.step_fn = self.step_expect_operator
+
+        elif next_char in whitespace:
+            pass
+        
+        else:
+            raise ValueError
+        
+        if len(self.tape) == 0 and self.cur_token != "":
+            self.tokens.append(self.cur_token)
+
+    def step_expect_operator(self):
+        next_char = self.tape[0]
+        self.tape = self.tape[1:]
+        if next_char in operators:
+            if next_char == close_bracket:
+                self.brackets_open -= 1
+
+            self.tokens.append(next_char)
+            self.step_fn = self.step_neutral
+        elif next_char in whitespace:
+            return
+        else:
+            raise ValueError
 
     def lex(self, input : str):
         self.reset()
+        self.tape = input
 
-        while len(self.input) > 0:
-            self.step()
+        while len(self.tape) > 0:
+            self.step_fn()
         
+        if self.brackets_open != 0:
+            raise ValueError("Mismatched brackets in expression.")
+
         return self.tokens
 
-print(ord("0"))
-print(ord("9"))
+class InfixParser():
+
+
+    def parse(self, tokens : list[str]):
+        if len(tokens) == 0:
+            return 0
+        
+
+luthor = InfixLexer()
+tokens = luthor.lex("(((10 + 10) * -5.32 * 20 + 5))")
+print(tokens)
